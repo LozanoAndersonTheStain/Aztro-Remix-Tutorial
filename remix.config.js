8ee4154a1b3256de58e93ad4ba12caf77720ac66
Anderson Lozano <adslozanov@gmail.com>
2025-01-26 22:06:02 -0500

    :truck: Modified routes

--- remix.config.js
+++ remix.config.js
@@ -1,12 +1,17 @@
 module.exports = {
+  appDirectory: "app",
+  outputDirectory: "public/build",
   future: {
     v2_routeConvention: true,
   },
+  ignoredRouteFiles: ["**/*.css, **/*.test.*"],
+  publicPath: "/build/",
   routes: async (defineRoutes) => {
     return defineRoutes((route) => {
       route("/", "routes/index.tsx");
-      route("/home", "routes/home.tsx");
       route("/movies/:movieId", "routes/movies.$movieId.tsx");
+      route("/movies/:movieId/edit", "routes/movies.$movieId_.edit.tsx");
     });
   },
+  serverBuildPath: "build/index.js",
 };
